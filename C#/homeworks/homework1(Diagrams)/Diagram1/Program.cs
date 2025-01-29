167adb299b248223d89bc1e797c4941e7a230a14
cAfton <166216153+cAfton@users.noreply.github.com>
2025-01-29 21:42:12 +0200

    fixed 2

--- C#/homeworks/homework1(Diagrams)/Diagram1/Program.cs
+++ C#/homeworks/homework1(Diagrams)/Diagram1/Program.cs
@@ -23,7 +23,17 @@ namespace Diagram1
         }
         static void Main(string[] args)
         {
-            int[] array = new int[5] {10, 3, 2, 7, 9 };
+            Console.Write("Size of array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("new number: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
 
             int min = findMin(array);
 
