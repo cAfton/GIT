00d783ff313186f472a871c400016ac26adb1d9c
cAfton <166216153+cAfton@users.noreply.github.com>
2025-02-20 21:55:53 +0200

    done

---
+++ C#/homeworks/homework5(interfaces)/CalculateHW/Interfaces/ICalc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateHW.Interfaces
+{
+    internal interface ICalc
+    {
+        int Less(int valueToCompare);
+
+        int Greater(int valueToCompare);
+
+        int CountDistinct();
+
+        int EqualToValue(int valueToCompare);
+    }
+}
