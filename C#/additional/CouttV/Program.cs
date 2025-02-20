c32c00f537608cef92c20de69ee905e8bfb6da96
cAfton <166216153+cAfton@users.noreply.github.com>
2025-02-20 23:01:20 +0200

    cout

---
+++ C#/additional/CouttV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouttV
+{
+    public class Cout
+    {
+        public static string operator <<(Cout c, string line)
+        {
+            Console.WriteLine(line);
+            return line;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cout cout = new Cout();
+            _ = cout << "HHHH";
+            Console.ReadLine();
+        }
+    }
+}
