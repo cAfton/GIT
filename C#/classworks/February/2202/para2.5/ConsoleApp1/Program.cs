eb72a3f009a1e3fb66b7807edf99721b2dd207ce
Inazer <nazarromaniv671@gmail.com>
2025-02-22 11:09:59 +0200

    ExtensionMethod

---
+++ C#/classworks/February/2202/para2.5/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    public static class ExtensionMethod
+    {
+        public static bool isOdd(this Int32 elem) 
+        {
+            return (elem % 2) == 0;
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+
+            Console.WriteLine(number.isOdd());
+            Console.ReadLine();
+        }
+    }
+}
