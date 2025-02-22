d13d0853574c548642e3a1757226f85d61f73032
Inazer <nazarromaniv671@gmail.com>
2025-02-22 10:13:49 +0200

    Finished

--- C#/classworks/February/2202/para1.5/AnonimFunction/Program.cs
+++ C#/classworks/February/2202/para1.5/AnonimFunction/Program.cs
@@ -11,17 +11,62 @@ namespace AnonimFunction
     {
         static void Main(string[] args)
         {
-            Func<int, string> Even = (elem) => {
-                if (elem % 2 == 1)
+            if (false)
+            {
+                #region FuncEven/NotEven
+                Func<int, string> Even = (elem) =>
                 {
-                    return "Not PaReNe";
+                    if (elem % 2 == 1)
+                    {
+                        return "Not PaReNe";
 
+                    }
+                    else { return "PaReNe"; }
+
+                };
+                Console.WriteLine(Even(5));
+                Console.ReadLine();
+                #endregion
+
+                #region Filter
+                Random rand = new Random();
+                List<int> list = new List<int>();
+                for (int i = 0; i < 10; i++)
+                {
+                    list.Add(rand.Next() % 10 - 5);
                 }
-                else { return "PaReNe"; }
 
-            };
-            Console.WriteLine(Even(5));
-            Console.ReadLine();
+                Func<List<int>, List<int>> Filter = (arr) =>
+                {
+                    List<int> arrFiltered = new List<int>();
+                    foreach (var item in arr)
+                    {
+                        if (item % 2 == 1)
+                        {
+                            arrFiltered.Add(item);
+                        }
+                    }
+                    return arrFiltered;
+                };
+
+                foreach (var item in Filter(list))
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadLine();
+                #endregion
+
+                #region Power
+
+                Func<int, int, double> pow = (item1, item2) =>
+                {
+                    return Math.Pow(item1, item2);
+                };
+
+                Console.WriteLine(pow(32, -2));
+                Console.ReadLine();
+                #endregion
+            }
         }
     }
 }
