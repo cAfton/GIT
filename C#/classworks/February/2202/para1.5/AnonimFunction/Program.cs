5ba7991924eda3e6efce131443308b52c2f83077
Inazer <nazarromaniv671@gmail.com>
2025-02-22 10:12:29 +0200

    Filter and power added

---
+++ C#/classworks/February/2202/para1.5/AnonimFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonimFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, string> Even = (elem) => {
+                if (elem % 2 == 1)
+                {
+                    return "Not PaReNe";
+
+                }
+                else { return "PaReNe"; }
+
+            };
+            Console.WriteLine(Even(5));
+            Console.ReadLine();
+        }
+    }
+}
