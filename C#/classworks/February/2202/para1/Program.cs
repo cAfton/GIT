47b8958886d946778e171666de0b1d7a1ff864c4
cAfton <166216153+cAfton@users.noreply.github.com>
2025-02-22 09:36:39 +0200

    Lambda

--- C#/classworks/February/2202/para1/Program.cs
+++ C#/classworks/February/2202/para1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,34 @@ namespace para1
     {
         static void Main(string[] args)
         {
+            int size = int.Parse(Console.ReadLine());
+            List<int> arr = new List<int>();
+            for (int i = 0; i < size; i++) {
+                arr.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Func<List<int>, (int, int)> max = (ara) => {
+                int t = ara[0];
+                int indexT = 0, index = 0;
+                foreach (var item in ara)
+                {
+                    if (item > t) 
+                    {
+                        t = item;
+                        index = indexT;
+                    }
+                    indexT++;
+                }
+
+                return (t, index);
+            };
+
+            var res = max(arr);
+
+            Console.WriteLine(res.Item1);
+            Console.WriteLine(res.Item2);
+
+            Console.ReadLine();
         }
     }
 }
