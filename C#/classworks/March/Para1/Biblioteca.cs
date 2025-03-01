a29996c43362cbe42ecd9d58aba60fde9f631318
cAfton <166216153+cAfton@users.noreply.github.com>
2025-03-01 11:26:04 +0200

    no

---
+++ C#/classworks/March/Para1/Biblioteca.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Para1
+{
+    public class HandWrite
+    {
+        public string Name {  get; set; }
+
+        public string Author { get; set; }   
+
+        public string Ganre {  get; set; }
+
+        public int PublishYear { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nAuthor: {Author}\nGanre: {Ganre}\nPublish year: {PublishYear}";
+        }
+    }
+
+    public class Book: HandWrite
+    {
+
+    }
+
+    public class Magasine: HandWrite
+    {
+        public int NumberOfPublish {  get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nNumber of publishing: {NumberOfPublish}";
+        }
+    }
+
+
+    public class Biblioteca<T> : IEnumerable<T> where T : HandWrite
+    {
+        public List<T> books { get; set; }
+        public IEnumerator<T> GetEnumerator()
+        {
+            //for (int i = 0; i < books.Count; i++)
+            //{
+            //    yield return books[i];
+            //}
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public List<T> FindGanre(string findGanre)
+        {
+            List<T> list = books.FindAll(elem => findGanre == elem.Ganre);
+            
+            return list;
+        }
+
+        public IEnumerable<T> FindYear(int findYear)
+        {
+            return books
+                .Where(elem => elem.PublishYear == findYear)
+                .OrderBy(elem => elem.PublishYear);
+        }
+
+        public IEnumerable<T> FindYearIteratated(int findYear)
+        {
+            List<T> list = books
+                .Where(elem => elem.PublishYear == findYear)
+                .OrderBy(elem => elem.PublishYear)
+                .ToList();
+
+            foreach (var item in list)
+            {
+                yield return item;
+            }
+        }
+
+        public IEnumerator<T> FindYearIterator(int findYear)
+        {
+            List<T> list = books
+                .Where(elem => elem.PublishYear == findYear)
+                .OrderBy(elem => elem.PublishYear)
+                .ToList();
+
+            return list.GetEnumerator();
+        }
+    }
+}
--- C#/classworks/March/Para1/Program.cs
+++ C#/classworks/March/Para1/Program.cs
@@ -21,7 +21,7 @@ namespace Para1
 
     public class Garage : IEnumerable<mafuncu>     //клас Гараге
     {
-        public List<mafuncu> cars {  get; set; } = new List<mafuncu>();
+        public List<mafuncu> cars { private get; set; } = new List<mafuncu>();
 
         public IEnumerator<mafuncu> GetEnumerator()
         {
@@ -33,7 +33,7 @@ namespace Para1
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
     }
 
@@ -48,16 +48,50 @@ namespace Para1
             {
                 cars = new List<mafuncu>() { new mafuncu(), new mafuncu(), new mafuncu()}
             };
-            
-            
 
 
-
-
-            foreach (var car in gar.cars)
+            foreach (var car in gar)
             {
                 Console.WriteLine($"{car.name}, {car.size}");
             }
+
+            Biblioteca<HandWrite> library = new Biblioteca<HandWrite>() { books = new List<HandWrite>() { 
+                new Book() {Name = "name1", Author = "Author1", Ganre = "Ganre1", PublishYear = 2000 }, 
+                new Book() {Name = "name12", Author = "Author3", Ganre = "Ganre2", PublishYear = 2012 }, 
+                new Book() {Name = "name13", Author = "Author3", Ganre = "Ganre3", PublishYear = 2019 }, 
+                new Book() {Name = "name14", Author = "Author3", Ganre = "Ganre2", PublishYear = 2023 }, 
+                new Book() {Name = "name15", Author = "Author1", Ganre = "Ganre4", PublishYear = 2007 }, 
+                new Magasine() {Name = "name2", Author = "Author2", Ganre = "Ganre2", PublishYear = 2000, NumberOfPublish = 12}, 
+                new Magasine() {Name = "name3", Author = "Author1", Ganre = "Ganre3", PublishYear = 2011, NumberOfPublish = 13}, 
+                new Magasine() {Name = "name4", Author = "Author1", Ganre = "Ganre4", PublishYear = 2014, NumberOfPublish = 14}, 
+                new Magasine() {Name = "name5", Author = "Author2", Ganre = "Ganre5", PublishYear = 2020, NumberOfPublish = 15}, 
+                new Magasine() {Name = "name6", Author = "Author1", Ganre = "Ganre2", PublishYear = 2020, NumberOfPublish = 16}, 
+                new Magasine() {Name = "name7", Author = "Author2", Ganre = "Ganre1", PublishYear = 2011, NumberOfPublish = 17}, 
+            } };
+
+            foreach (var item in library)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+
+            List<HandWrite> list = library.FindYear(2000);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (var item in library.FindYearIteratated(2000))
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+            var iterator = library.FindYearIterator(2000);
+            do
+            {
+                Console.WriteLine(iterator.Current);
+            } while (iterator.MoveNext());
+
             Console.ReadLine();
         }
     }
