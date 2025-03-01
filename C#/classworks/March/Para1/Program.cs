fa163f4b4a68166fd15476373e925ed943fdc34b
Inazer <nazarromaniv671@gmail.com>
2025-03-01 09:31:38 +0200

    Start

---
+++ C#/classworks/March/Para1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Numerics;
+
+namespace Para1
+{
+
+
+    public class mafuncu //Мафинки
+    {
+        public string name {  get; set; }
+
+        public int size {  get; set; }
+
+        mafuncu()
+        {
+            name = "Mafunku!";
+            size = 0;
+        }
+
+    }
+
+    public class Garage : IEnumerable<mafuncu>     //клас Гараге
+    {
+        public List<mafuncu> cars {  get; set; } = new List<mafuncu>();
+
+        public IEnumerator<mafuncu> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
