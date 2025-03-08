cbd77853ae222ea59096ce00bde860b003f66001
cAfton <166216153+cAfton@users.noreply.github.com>
2025-03-08 13:34:30 +0200

    ,

---
+++ C#/classworks/March/0803/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    public enum num
+    {
+        fkjskjfd,
+        lshfjkre,
+        efref
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(num.efref.ToString());
+        }
+    }
+}
