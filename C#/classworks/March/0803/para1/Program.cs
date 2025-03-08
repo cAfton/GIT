d8d24f7668ba9f572c47d96ae63f325d24f0deec
cAfton <166216153+cAfton@users.noreply.github.com>
2025-03-08 09:29:58 +0200

    q

---
+++ C#/classworks/March/0803/para1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace para1
+{
+    public class Performance : IDisposable
+    {
+        public string Name { get; set; }
+        public string Theater {  get; set; }
+
+        public string Janre {  get; set; }
+
+        public List<string> Actors { get; set; }
+        public Performance() {
+            Name = "name";
+            Theater = "theater";
+            Janre = "janre";
+            Actors = new List<string>();
+        }
+        public Performance(string name, string theater, string janre) {
+            Name = name;
+            Theater = theater;
+            Janre = janre;
+            Actors = new List<string>();
+        }
+
+        public void Dispose()
+        {
+            Actors.Clear();
+            Console.WriteLine("Dispose done");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Performance perf = new Performance();
+            Console.WriteLine(perf.Name);
+        }
+    }
+}
