475051c4f6a247644ebdf61627f985b6ee711f33
cAfton <166216153+cAfton@users.noreply.github.com>
2025-03-15 12:24:13 +0200

    done

---
+++ C#/classworks/March/1503/task4(para2)/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace task4_para2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() {"apple", "banana", "cherry", "dog", "elephant", "forest", "guitar", "house", "island", "jungle"};
+            Console.WriteLine("1 - by Increasing\n2 - Decreasing");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    list = list.OrderBy(elem => elem.Length).ToList();
+                    break;
+                case "2":
+                    list = list.OrderByDescending(elem => elem.Length).ToList();
+                    break;
+                default:
+                    break;
+            }
+            list.ForEach(elem => Console.WriteLine(elem));
+        }
+    }
+}
