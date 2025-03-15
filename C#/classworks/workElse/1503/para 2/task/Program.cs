039dcbfcb37d4acf09b518ef8d8aca259f23b004
Inazer <nazarromaniv671@gmail.com>
2025-03-15 11:22:53 +0200

    taskV add

---
+++ C#/classworks/workElse/1503/para 2/task/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "apple", "banana" };
+            Console.WriteLine("1 - >\n2 - <");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    list = list.OrderBy(x => x.Length).ToList();
+                    break;
+                case 2:
+                    list = list.OrderByDescending(x => x.Length).ToList();
+                    break;
+                default:
+                    break;
+            }
+
+
+            list.OrderBy(elem => elem.Length).ToList();
+            list.ForEach(elem => Console.WriteLine(elem));
+
+        }
+    }
+}
