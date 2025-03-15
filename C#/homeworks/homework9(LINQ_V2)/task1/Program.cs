5c2242ccab1600af4b9210f164d2b78462c78003
cAfton <166216153+cAfton@users.noreply.github.com>
2025-03-15 14:02:08 +0200

    Task1 done

---
+++ C#/homeworks/homework9(LINQ_V2)/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> surnames = new List<string> { "Smith", "Johnson", "Brown", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin" };
+            Console.WriteLine(surnames.All(elem => elem.Length > 3));
+            Console.WriteLine(surnames.All(elem => elem.Length > 3 && elem.Length < 10));
+            Console.WriteLine(surnames.Any(elem => elem.StartsWith("W")));
+            Console.WriteLine(surnames.Any(elem => elem.EndsWith("y")));
+            Console.WriteLine(surnames.Contains("Orange"));
+            Console.WriteLine(surnames.FirstOrDefault(elem => elem.Length == 6));
+            Console.WriteLine(surnames.LastOrDefault(elem => elem.Length < 15));
+        }
+    }
+}
