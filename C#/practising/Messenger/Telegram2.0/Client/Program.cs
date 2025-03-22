aa81b92b6382206ead78dbcfd37a4b5452d5d820
cAfton <166216153+cAfton@users.noreply.github.com>
2025-03-22 09:38:22 +0200

    droped

---
+++ C#/practising/Messenger/Telegram2.0/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 5000);
+        NetworkStream stream = client.GetStream();
+
+        while (true)  // Цикл для безперервного вводу
+        {
+            Console.Write("Enter message: ");
+            string message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message)) break;  // Вихід з циклу, якщо нічого не введено
+
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+
+        client.Close();
+    }
+}
---
+++ C#/practising/Messenger/Telegram2.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+        Console.WriteLine("Server started...");
+
+        TcpClient client = server.AcceptTcpClient();
+        NetworkStream stream = client.GetStream();
+
+        while (true)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            if (bytesRead == 0) break;
+
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received: " + message);
+        }
+
+        client.Close();
+        server.Stop();
+    }
+}
