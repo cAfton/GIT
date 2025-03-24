c8f120c7ad7bcd141705b216e79f48efaac74f85
cAfton <166216153+cAfton@users.noreply.github.com>
2025-03-24 23:32:17 +0200

    task2 base

---
+++ C#/homeworks/homework10(Pattern)/Task2/ShapesProga/Factories/ShapeFactory.cs
@@ -0,0 +1,14 @@
+﻿using ShapesProga.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesProga.Factories
+{
+    public abstract class ShapeFactory
+    {
+        public abstract IShape CreateShape();
+    }
+}
---
+++ C#/homeworks/homework10(Pattern)/Task2/ShapesProga/Program.cs
@@ -0,0 +1,21 @@
+﻿using ShapesProga.Factories;
+using ShapesProga.Interface;
+using ShapesProga.Model;
+
+namespace ShapesProga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory circleFactory = new CircleFactory();
+            IShape circle = circleFactory.CreateShape();
+            circle.Draw();
+
+            ShapeFactory rectangleFactory = new RectangleFactory();
+            IShape rectangle = rectangleFactory.CreateShape();
+            rectangle.Draw();
+
+        }
+    }
+}
