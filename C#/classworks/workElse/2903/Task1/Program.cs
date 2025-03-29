e35e48b0f6b74c732301a016765374ae13f69cf7
cAfton <166216153+cAfton@users.noreply.github.com>
2025-03-29 10:57:52 +0200

    TaskManeger done

---
+++ C#/classworks/workElse/2903/Task1/Program.cs
@@ -0,0 +1,172 @@
+﻿using Task1.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Task1
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string json;
+            string jsonFromFile = File.ReadAllText("Tasks.json");
+            Dictionary<string, TaskMeneger> Tasks = JsonConvert.DeserializeObject<Dictionary<string, TaskMeneger>>(jsonFromFile, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+
+            while (true)
+            {
+                Console.WriteLine("1 - Add new list\n2 - Add new task\n3 - Update task\n4 - Delete task\n5 - Print all lists\n6 - Print one list \n7 - Find task in list\n8 - Find task in all lists\n9 - Save all\n0 - exit");
+                switch(Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Enter name of new list: ");
+                        Tasks.Add(Console.ReadLine(), new TaskMeneger());
+                        break;
+                    case "2":
+
+                        foreach (var item in Tasks)
+                        {
+                            Console.WriteLine(item.Key);
+                        }
+                        Console.WriteLine("\nEnter name of list: ");
+                        string listToAdd = Console.ReadLine();
+
+                        TaskToDO newTask = new TaskToDO();
+                        Console.WriteLine("\nEnter name of task: ");
+                        newTask.Name = Console.ReadLine();
+
+                        Console.WriteLine("\nEnter description: ");
+                        newTask.Description = Console.ReadLine();
+
+                        Console.WriteLine("\nEnter priority:\n 1 - High\n2 - Medium\n3 - Low");
+                        switch (Console.ReadLine())
+                        {
+                            case "1":
+                                newTask.TaskPriority = Priority.High;
+                                break;
+                            case "2":
+                                newTask.TaskPriority = Priority.Medium;
+                                break;
+                            case "3":
+                                newTask.TaskPriority = Priority.Low;
+                                break;
+                            default:
+                                newTask.TaskPriority = Priority.Low;
+                                break;
+                        }
+
+                        Console.WriteLine("\nEnter dead line: ");
+                        newTask.DeadLine = DateTime.Parse(Console.ReadLine());
+
+
+                        try
+                        {
+                            Tasks[listToAdd].newTask(newTask);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "3":
+                        foreach (var item in Tasks)
+                        {
+                            Console.WriteLine(item.Key);
+                        }
+                        Console.WriteLine("\nEnter name of list: ");
+                        try
+                        {
+                            Tasks[Console.ReadLine()].UpdateTask();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "4":
+                        foreach (var item in Tasks)
+                        {
+                            Console.WriteLine(item.Key);
+                        }
+                        Console.WriteLine("\nEnter name of list: ");
+                        try
+                        {
+                            Tasks[Console.ReadLine()].DeleteTask();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "5":
+                        foreach (var item in Tasks)
+                        {
+                            Console.WriteLine($"List: {item.Key}");
+                            foreach(var taske in item.Value.TaskList)
+                            {
+                                Console.WriteLine(taske);
+                            }
+                        }
+                        break;
+                    case "6":
+                        foreach (var item in Tasks)
+                        {
+                            Console.WriteLine(item.Key);
+                        }
+                        Console.WriteLine("\nEnter name of list: ");
+                        try
+                        {
+                            foreach(var item in Tasks[Console.ReadLine()].TaskList)
+                            {
+                                Console.WriteLine(item);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "7":
+                        foreach (var item in Tasks)
+                        {
+                            Console.WriteLine(item.Key);
+                        }
+                        Console.WriteLine("\nEnter name of list: ");
+                        try
+                        {
+                            var found = Tasks[Console.ReadLine()].SearchTask();
+                            Console.WriteLine((found == null ? "Didnt find" : found));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "8":
+                        foreach (var item in Tasks)
+                        {
+                            string list = item.Key;
+                            var found = item.Value.SearchTask();
+                            if (found != null)
+                            {
+                                Console.WriteLine($"List: {list}\nTask: {found}");
+                                break;
+                            }
+                        }
+                        break;
+                    case "9":
+                        json = JsonConvert.SerializeObject(Tasks, Formatting.Indented, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+                        File.WriteAllText("Tasks.json", json);
+                        break;
+                    case "0":
+                        json = JsonConvert.SerializeObject(Tasks, Formatting.Indented, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+                        File.WriteAllText("Tasks.json", json);
+                        return;
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ C#/classworks/workElse/2903/Task1/TaskMeneger.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Models;
+
+namespace Task1
+{
+    public class TaskMeneger
+    {
+
+        public List<TaskToDO> TaskList { get; set; } = new List<TaskToDO> { };
+
+        public void newTask(TaskToDO newTask) {
+            TaskList.Add(newTask);
+        }
+
+        public void UpdateTask() {
+            Console.WriteLine("Enter name of task: ");
+            string findTask = Console.ReadLine();
+            TaskToDO task = TaskList.FirstOrDefault(elem => elem.Name == findTask);
+            if (task != null)
+            {
+                Console.WriteLine("Update:\n1 - Name\n2 - Description\n3 - Priority\n4 - Dead line");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        task.Name = Console.ReadLine();
+                        break;
+                    case "2":
+                        task.Description = Console.ReadLine();
+                        break;
+                    case "3":
+                        Console.WriteLine("New priority:\n1 - High\n2 - Medium\n3 - Low");
+                        switch (Console.ReadLine())
+                        {
+                            case "1":
+                                task.TaskPriority = Priority.High;
+                                break;
+                            case "2":
+                                task.TaskPriority = Priority.Medium;
+                                break;
+                            case "3":
+                                task.TaskPriority = Priority.Low;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine("enter new dead line: ");
+                        task.DeadLine = DateTime.Parse(Console.ReadLine());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Task didnt find");
+            }
+            
+        }
+        public void DeleteTask()
+        {
+            Console.WriteLine("Enter name of task: ");
+            string findTask = Console.ReadLine();
+            TaskList.Remove(TaskList.FirstOrDefault(elem => elem.Name == findTask));
+        }
+
+        public TaskToDO SearchTask() {
+            Console.WriteLine("Enter name of task: ");
+            string findTask = Console.ReadLine();
+            return TaskList.FirstOrDefault(elem => elem.Name == findTask);
+        }
+
+        public void PrintEverything() {
+            TaskList.ForEach(elem => Console.WriteLine(elem));
+        }
+
+        public void SortByPriority()
+        {
+            TaskList.OrderBy(elem => elem.TaskPriority);
+        }
+        public void SortByDeadLine()
+        {
+            TaskList.OrderBy(elem => elem.DeadLine);
+        }
+    }
+}
