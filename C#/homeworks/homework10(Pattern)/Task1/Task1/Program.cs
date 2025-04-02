780ff1e7d3e4e70115610f0548fdcb764b754f8d
Inazer <nazarromaniv671@gmail.com>
2025-04-02 23:34:53 +0300

    finished

--- C#/homeworks/homework10(Pattern)/Task1/Task1/Program.cs
+++ C#/homeworks/homework10(Pattern)/Task1/Task1/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Task1
+﻿using Task1.Main_class;
+
+namespace Task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AppConfig appConfig = AppConfig.Instance;
+            appConfig.SetSetting("Theme", "Dark");
+            appConfig.SetSetting("Language", "English");
+
+            Console.WriteLine(appConfig.GetSetting("Theme")); // Dark
+            Console.WriteLine(appConfig.GetSetting("Language")); // English
         }
     }
 }
