41a40223fb2c83bb394cedffdaecf1a7c9439dcd
cAfton <166216153+cAfton@users.noreply.github.com>
2025-04-05 14:54:46 +0300

    done but didnt tested

---
+++ C#/homeworks/homework11(Preparation)/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(1, 1, 1);
+            DateTime dateTime1 = new DateTime(1, 1, 1);
+
+            Console.WriteLine(dateTime == dateTime1);
+
+            return;
+            Task_Manager.Menu();
+        }
+    }
+}
