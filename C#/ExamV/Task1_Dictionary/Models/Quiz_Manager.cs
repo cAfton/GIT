aadc78d391d71eeb1d2ea9494bbc54fddb5b12dc
cAfton <166216153+cAfton@users.noreply.github.com>
2025-04-05 11:37:47 +0300

    task1 done

---
+++ C#/ExamV/Task1_Dictionary/Models/Quiz_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Dictionary.Models
+{
+    public class Quiz_Manager
+    {
+        public List<Quiz> Quizes_Easy { get; set; }
+        public List<Quiz> Quizes_Medium { get; set; }
+        public List<Quiz> Quizes_Hard { get; set; }
+    }
+}
