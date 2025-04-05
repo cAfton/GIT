cf736fc3fdbc57f7447adb28fd544ba1aab7c64a
cAfton <166216153+cAfton@users.noreply.github.com>
2025-04-05 12:52:18 +0300

    task1 done

---
+++ C#/ExamV/task1_RealDictionary/Models/LanguageDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_RealDictionary.Models
+{
+    internal class LanguageDictionary
+    {
+        public Dictionary<string, List<string>> DictionaryLang { get; set; } = new Dictionary<string, List<string>> ();
+    }
+}
---
+++ C#/ExamV/task1_RealDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using task1_RealDictionary.Models;
+
+namespace task1_RealDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            task_manager.Menu();
+        }
+    }
+}
