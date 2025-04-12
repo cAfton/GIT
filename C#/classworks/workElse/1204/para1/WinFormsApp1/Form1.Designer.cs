de75a0c8d54f7d2571b95fc2f018329b2071d0e2
Inazer <nazarromaniv671@gmail.com>
2025-04-12 09:42:53 +0300

    start

---
+++ C#/classworks/workElse/1204/para1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "tomato", "potato" });
+            listBox1.Location = new Point(27, 106);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(254, 224);
+            listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(300, 106);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(254, 224);
+            listBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Word";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(473, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Translation";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(413, 362);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 51);
+            button1.TabIndex = 4;
+            button1.Text = "Add new word";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+    }
+}
