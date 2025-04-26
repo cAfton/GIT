53e3cccfc184c50663a56276e72f01d53df3f2b1
oleksii103 <132712296+oleksii103@users.noreply.github.com>
2025-04-26 14:51:03 +0300

    oleksii

---
+++ C#/classworks/April/1204/taska2/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taska2
+{
+    public class Result
+    {
+        public DateTime Time { get; set; }
+        public string UserName { get; set; }
+        public int Score { get; set; }
+        public TypesOfQuiz TypesOfQuiz { get; set; }
+        public HardLevel HardLevel { get; set; }
+        public int NumberOfQestions { get; set; }
+    }
+}
