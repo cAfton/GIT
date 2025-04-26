daeb6d4aa777e53b56ecf89c5851944bb20a7a84
Inazer <nazarromaniv671@gmail.com>
2025-04-26 09:11:58 +0300

    Шось

---
+++ C#/classworks/workElse/1204/para2/WinFormsApp1/Form1.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
