e12a7fd897c83b1b3cf09a3bdb04ee0e80affa76
Inazer <nazarromaniv671@gmail.com>
2025-05-03 14:35:59 +0300

    Shop start

--- C#/classworks/May/0305/Shop/ShopPictures/Form1.Designer.cs
+++ C#/classworks/May/0305/Shop/ShopPictures/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(776, 310);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(40, 343);
+            button1.Name = "button1";
+            button1.Size = new Size(241, 95);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
     }
 }
--- C#/classworks/May/0305/Shop/ShopPictures/Form1.cs
+++ C#/classworks/May/0305/Shop/ShopPictures/Form1.cs
@@ -1,10 +1,36 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+
 namespace ShopPictures
 {
     public partial class Form1 : Form
     {
+        private DriveService driveService;
         public Form1()
         {
             InitializeComponent();
+            InitDriveService();
+        }
+        private void InitDriveService()
+        {
+            var credential = GoogleCredential.FromFile("shoponwinformscs-de54ab3b9543.json").CreateScoped(DriveService.Scope.DriveReadonly);
+
+            driveService = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Drive Image Loader Service",
+            });
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileId = "1eC3uyEmquCRufx-Md1XeY0i_7OjCj9Ar";
+            var request = driveService.Files.Get(fileId);
+            var stream = new MemoryStream();
+            request.Download(stream);
+            stream.Position = 0;
+
+            pictureBox1.Image = Image.FromStream(stream);
         }
     }
 }
