9155ca296b8b18ef76af07ed55542e745cb0db7a
cAfton <166216153+cAfton@users.noreply.github.com>
2025-05-09 21:07:05 +0300

    done

---
+++ C#/homeworks/!WindowsFormsHomework/homework3(V3)/Task2/EditForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task2;
+using Task2.Models;
+
+namespace Task2
+{
+    public partial class EditForm : Form
+    {
+        private int Type { get; set; }
+
+        public List<Product> Upd_Products { get; set; }
+        public EditForm(int type, List<Product> list)
+        {
+            InitializeComponent();
+            Type = type;
+            Upd_Products = list;
+
+            comboBox1.Items.Clear();
+            foreach (var item in list)
+            {
+                comboBox1.Items.Add(item.Name);
+            }
+
+            switch (type)
+            {
+                case 1:
+                    comboBox1.Visible = false;
+                    button1.Text = "Add";
+                    break;
+                case 2:
+                    button1.Text = "Save";
+                    break;
+                case 3:
+                    groupBox_Edit.Visible = false;
+                    button1.Text = "Delete";
+                    break;
+
+                default:
+                    Close();
+                    break;
+            }
+
+            button1.Enabled = false;
+        }
+
+
+        private void AddNew()
+        {
+            Upd_Products.Add(new Product { Name = textBox_Name.Text, Characteristic = textBox_Characteristic.Text, Description = richTextBox_Description.Text, Cost = (double)numericUpDown_Cost.Value });
+        }
+
+        private void Delete()
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                Upd_Products.Remove(Upd_Products[comboBox1.SelectedIndex]);
+            }
+        }
+
+        private void Edit()
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                Upd_Products[comboBox1.SelectedIndex].Name = textBox_Name.Text;
+                Upd_Products[comboBox1.SelectedIndex].Characteristic = textBox_Characteristic.Text;
+                Upd_Products[comboBox1.SelectedIndex].Description = richTextBox_Description.Text;
+                Upd_Products[comboBox1.SelectedIndex].Cost = (double)numericUpDown_Cost.Value;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (Type)
+            {
+                case 1:
+                    AddNew();
+                    break;
+                case 2:
+                    Edit();
+                    break;
+                case 3:
+                    Delete();
+                    break;
+
+                default:
+                    Close();
+                    break;
+            }
+            DialogResult = DialogResult.OK;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox_Name.Text = Upd_Products[comboBox1.SelectedIndex].Name;
+            textBox_Characteristic.Text = Upd_Products[comboBox1.SelectedIndex].Characteristic;
+            richTextBox_Description.Text = Upd_Products[comboBox1.SelectedIndex].Description;
+            numericUpDown_Cost.Value = (decimal)Upd_Products[comboBox1.SelectedIndex].Cost;
+
+            button1.Enabled = true;
+
+        }
+    }
+}
---
+++ C#/homeworks/!WindowsFormsHomework/homework3(V3)/Task2/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            textBox_cost = new TextBox();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            textBox_BusketCost = new TextBox();
+            button1 = new Button();
+            menuStrip1 = new MenuStrip();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            addToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem1 = new ToolStripMenuItem();
+            deleteToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 36);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(363, 28);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // textBox_cost
+            // 
+            textBox_cost.Location = new Point(449, 37);
+            textBox_cost.Name = "textBox_cost";
+            textBox_cost.ReadOnly = true;
+            textBox_cost.Size = new Size(125, 27);
+            textBox_cost.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 188);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(363, 104);
+            listBox1.TabIndex = 2;
+            listBox1.MouseDoubleClick += listBox1_MouseDoubleClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 147);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 38);
+            label1.TabIndex = 3;
+            label1.Text = "Busket";
+            // 
+            // textBox_BusketCost
+            // 
+            textBox_BusketCost.Location = new Point(449, 188);
+            textBox_BusketCost.Name = "textBox_BusketCost";
+            textBox_BusketCost.ReadOnly = true;
+            textBox_BusketCost.Size = new Size(125, 27);
+            textBox_BusketCost.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 89);
+            button1.Name = "button1";
+            button1.Size = new Size(198, 35);
+            button1.TabIndex = 5;
+            button1.Text = "Add to busket";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { editToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(620, 28);
+            menuStrip1.TabIndex = 6;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { addToolStripMenuItem, editToolStripMenuItem1, deleteToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(58, 24);
+            editToolStripMenuItem.Text = "Edit...";
+            // 
+            // addToolStripMenuItem
+            // 
+            addToolStripMenuItem.Name = "addToolStripMenuItem";
+            addToolStripMenuItem.Size = new Size(224, 26);
+            addToolStripMenuItem.Text = "Add";
+            addToolStripMenuItem.Click += addToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem1
+            // 
+            editToolStripMenuItem1.Name = "editToolStripMenuItem1";
+            editToolStripMenuItem1.Size = new Size(224, 26);
+            editToolStripMenuItem1.Text = "Edit";
+            editToolStripMenuItem1.Click += editToolStripMenuItem1_Click;
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.Size = new Size(224, 26);
+            deleteToolStripMenuItem.Text = "Delete";
+            deleteToolStripMenuItem.Click += deleteToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(620, 305);
+            Controls.Add(button1);
+            Controls.Add(textBox_BusketCost);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox_cost);
+            Controls.Add(comboBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private TextBox textBox_cost;
+        private ListBox listBox1;
+        private Label label1;
+        private TextBox textBox_BusketCost;
+        private Button button1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem addToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem1;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
