735980be259e9fb6f3e9eddcb7c11ee3562415d9
cAfton <166216153+cAfton@users.noreply.github.com>
2025-05-09 18:33:22 +0300

    done 1 and 3

---
+++ C#/homeworks/!WindowsFormsHomework/homework3(L3)/Task3/EditForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class EditForm : Form
+    {
+        public string EditedText { get; set; } = "";
+        public EditForm(string text)
+        {
+            InitializeComponent();
+            richTextBox_Edited.Text = text;
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            EditedText = richTextBox_Edited.Text;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void button_Cancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
