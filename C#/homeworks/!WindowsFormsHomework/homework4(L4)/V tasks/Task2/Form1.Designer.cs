f32c9690115728dd365f7f648686ecc0d2c3288f
cAfton <166216153+cAfton@users.noreply.github.com>
2025-05-11 16:38:08 +0300

    task 2 done

--- C#/homeworks/!WindowsFormsHomework/homework4(L4)/V tasks/Task2/Form1.Designer.cs
+++ C#/homeworks/!WindowsFormsHomework/homework4(L4)/V tasks/Task2/Form1.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            treeView1 = new TreeView();
+            menuStrip1 = new MenuStrip();
+            addToolStripMenuItem = new ToolStripMenuItem();
+            deleteToolStripMenuItem = new ToolStripMenuItem();
+            textBox1 = new TextBox();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            listView1 = new ListView();
+            menuStrip1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new Point(12, 111);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(843, 392);
+            treeView1.TabIndex = 0;
+            treeView1.BeforeExpand += treeView1_BeforeExpand;
+            treeView1.AfterSelect += treeView1_AfterSelect;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { addToolStripMenuItem, deleteToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1198, 28);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // addToolStripMenuItem
+            // 
+            addToolStripMenuItem.Name = "addToolStripMenuItem";
+            addToolStripMenuItem.Size = new Size(51, 24);
+            addToolStripMenuItem.Text = "Add";
+            addToolStripMenuItem.Click += add_Click;
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.Size = new Size(67, 24);
+            deleteToolStripMenuItem.Text = "Delete";
+            deleteToolStripMenuItem.Click += delete_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(1174, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2 });
+            toolStrip1.Location = new Point(0, 28);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1198, 27);
+            toolStrip1.TabIndex = 3;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(29, 24);
+            toolStripButton1.Text = "toolStripButton1";
+            toolStripButton1.Click += add_Click;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(29, 24);
+            toolStripButton2.Text = "toolStripButton2";
+            toolStripButton2.Click += delete_Click;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(861, 111);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(325, 392);
+            listView1.TabIndex = 4;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1198, 514);
+            Controls.Add(listView1);
+            Controls.Add(toolStrip1);
+            Controls.Add(textBox1);
+            Controls.Add(treeView1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Folder manager";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TreeView treeView1;
+        private MenuStrip menuStrip1;
+        private TextBox textBox1;
+        private ToolStrip toolStrip1;
+        private ListView listView1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripMenuItem addToolStripMenuItem;
+        private ToolStripMenuItem deleteToolStripMenuItem;
     }
 }
--- C#/homeworks/!WindowsFormsHomework/homework4(L4)/V tasks/Task2/Form1.cs
+++ C#/homeworks/!WindowsFormsHomework/homework4(L4)/V tasks/Task2/Form1.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.VisualBasic;
+using System.IO;
+
 namespace Task2
 {
     public partial class Form1 : Form
@@ -5,6 +8,84 @@ namespace Task2
         public Form1()
         {
             InitializeComponent();
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    TreeNode node = new TreeNode(drive.Name);
+                    node.Tag = drive.Name;
+                    node.Nodes.Add("");
+                    treeView1.Nodes.Add(node);
+                }
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+
+            try
+            {
+                List<string> insides = Directory.GetDirectories(e.Node.Tag.ToString()/*<-path*/).ToList();
+                foreach (string file in insides)
+                {
+                    TreeNode subNode = new TreeNode(Path.GetFileName(file));
+                    subNode.Tag = file;
+                    if (Directory.GetDirectories(file).Length > 0)
+                        subNode.Nodes.Add("");
+                    e.Node.Nodes.Add(subNode);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                e.Node.Nodes.Add("❌ Access denied!");
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listView1.Items.Clear();
+            textBox1.Text = e.Node.Tag.ToString();
+
+            try
+            {
+                foreach (string file in Directory.GetFiles(e.Node.Tag.ToString()))
+                {
+                    listView1.Items.Add(new ListViewItem(Path.GetFileName(file)));
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                e.Node.Nodes.Add("❌ Access denied!");
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //File.Delete(textBox1.Text);
+                Directory.Delete(textBox1.Text);
+                treeView1.SelectedNode.Remove();
+
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access denied!", ":(", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("The directory is not empty!", ":(", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            string name = Interaction.InputBox("Input name:", "Name");
+            Directory.CreateDirectory($"{textBox1.Text}\\{name}");
+            TreeNode newNode = new TreeNode(Path.GetFileName(name));
+            newNode.Tag = $"{textBox1.Text}\\{name}";
+            treeView1.SelectedNode.Nodes.Add(newNode);
         }
     }
 }
