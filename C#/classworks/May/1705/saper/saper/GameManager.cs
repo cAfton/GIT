0a1d1e95455abbe34ad324298bba65d142f1224c
Inazer <nazarromaniv671@gmail.com>
2025-05-17 14:53:21 +0300

    Saper start

---
+++ C#/classworks/May/1705/saper/saper/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saper
+{
+
+    public enum Status
+    {
+        Mina, Empty, Number, Flag
+    }
+    public class GameManager
+    {
+        public int MiNumberOfMin;
+        public const int Size = 10;
+        public Button[,] buttons = new Button[Size, Size];
+        private Control control;
+        GameManager(int Mins, Control control)
+        {
+            this.control = control;
+            MiNumberOfMin = Mins;
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    buttons[i, j] = new Button();
+                }
+            }
+        }
+        public void StartGame()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < MiNumberOfMin; i++)
+            {
+                //fix to not set mina in mina
+                buttons[random.Next(Size), random.Next(Size)].Tag = Status.Mina;
+            }
+
+        }
+
+    }
+}
