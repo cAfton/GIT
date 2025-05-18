ba619760629171a047fc962ba94c23735c89d50c
cAfton <166216153+cAfton@users.noreply.github.com>
2025-05-18 22:49:02 +0300

    V done

--- C#/homeworks/!WindowsFormsHomework/homework5(Painting)/V`s tasks/task1/Form1.Designer.cs
+++ C#/homeworks/!WindowsFormsHomework/homework5(Painting)/V`s tasks/task1/Form1.Designer.cs
@@ -28,7 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            infoToolStripMenuItem = new ToolStripMenuItem();
+            deleteToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { infoToolStripMenuItem, deleteToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(211, 80);
+            // 
+            // infoToolStripMenuItem
+            // 
+            infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            infoToolStripMenuItem.Size = new Size(210, 24);
+            infoToolStripMenuItem.Text = "Info";
+            infoToolStripMenuItem.Click += infoToolStripMenuItem_Click;
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.Size = new Size(210, 24);
+            deleteToolStripMenuItem.Text = "Delete";
+            deleteToolStripMenuItem.Click += deleteToolStripMenuItem_Click;
             // 
             // Form1
             // 
@@ -39,9 +65,14 @@
             Text = "Form1";
             Paint += Form1_Paint;
             Resize += Form1_Resize;
+            contextMenuStrip1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem infoToolStripMenuItem;
+        private ToolStripMenuItem deleteToolStripMenuItem;
     }
 }
--- C#/homeworks/!WindowsFormsHomework/homework5(Painting)/V`s tasks/task1/Form1.cs
+++ C#/homeworks/!WindowsFormsHomework/homework5(Painting)/V`s tasks/task1/Form1.cs
@@ -7,7 +7,7 @@ namespace task1
         private const int Squares = 8;
         private Label[,] labels = new Label[Squares, Squares];
 
-        private readonly string[,] board = new string[8, 8] {
+        private string[,] board = new string[8, 8] {
             { "♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜" },
             { "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟" },
             { "", "", "", "", "", "", "", "" },
@@ -21,30 +21,6 @@ namespace task1
         public Form1()
         {
             InitializeComponent();
-            float sideW = this.ClientSize.Width / (float)Squares;
-            float sideH = this.ClientSize.Height / (float)Squares;
-            for (int i = 0; i < Squares; i++)
-            {
-                for (int j = 0; j < Squares; j++)
-                {
-                    Point labelPoint = new Point((int)(sideW * i + 20), (int)(sideH * i + 20));
-                    labels[i, j] = new Label
-                    {
-                        Text = board[i, j],
-                        Font = new Font("Segoe UI Symbol", 28),
-                        Location = labelPoint,
-                        Tag = $"{Squares - j}{(char)('A' + (Squares - i - 1))}",
-                        AutoSize = true,
-                        Size = new Size(50, 20),
-
-                        BackColor = Color.Black/*((i + j) % 2 == 1 ? Color.Black : Color.White)*/,
-                        ForeColor = Color.White/*((i + j) % 2 == 1 ? Color.Black : Color.White)*/,
-
-                        Visible = true
-                    };
-                    this.Controls.Add(labels[i, j]);
-                }
-            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -60,16 +36,56 @@ namespace task1
                 for (int j = 0; j < Squares; j++)
                 {
                     Brush brush = ((i + j) % 2 == 0 ? Brushes.Black : Brushes.White);
-                    
+
                     graphics.DrawRectangle(p, sideW * i, sideH * j, sideW, sideH);
                     graphics.FillRectangle(brush, sideW * i, sideH * j, sideW, sideH);
+
+
+                    Point labelPoint = new Point((int)((sideW * j) + (this.ClientSize.Width * 0.02)), (int)((sideH * i) + (this.ClientSize.Height * 0.02)));
+                    labels[i, j] = new Label
+                    {
+                        Text = board[i, j],
+                        Font = new Font("Segoe UI Symbol", (int)(((this.ClientSize.Width * this.ClientSize.Height) * 0.000045))),
+                        Location = labelPoint,
+                        Tag = $"{Squares - j}{(char)('A' + i)}",
+                        AutoSize = true,
+                        Size = new Size(20, 20),
+
+                        BackColor = ((i + j) % 2 == 0 ? Color.Black : Color.White),
+                        ForeColor = ((i + j) % 2 == 1 ? Color.Black : Color.White),
+                        ContextMenuStrip = contextMenuStrip1
+                    };
+                    this.Controls.Add(labels[i, j]);
                 }
             }
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
+            this.Controls.Clear();
             Invalidate();
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Label clickedSquare = contextMenuStrip1.SourceControl as Label;
+            MessageBox.Show($"Piece: {clickedSquare.Text}\nLocation: {clickedSquare.Tag}", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Label clickedSquare = contextMenuStrip1.SourceControl as Label;
+
+            string tag = clickedSquare.Tag.ToString();
+            int x = tag[1] - 'A';
+            int y = Squares - int.Parse(tag[0].ToString());
+
+            board[x, y] = "";
+
+            Form1_Resize(sender, e);
+
+            MessageBox.Show($"Piece at location {clickedSquare.Tag} deleted", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
     }
 }
