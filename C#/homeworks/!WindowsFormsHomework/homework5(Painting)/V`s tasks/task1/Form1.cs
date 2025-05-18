0b053ed16027e22befbf791ae7da0e0cae7c2e07
cAfton <166216153+cAfton@users.noreply.github.com>
2025-05-18 15:33:20 +0300

    working

--- C#/homeworks/!WindowsFormsHomework/homework5(Painting)/V`s tasks/task1/Form1.cs
+++ C#/homeworks/!WindowsFormsHomework/homework5(Painting)/V`s tasks/task1/Form1.cs
@@ -1,12 +1,50 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace task1
 {
     public partial class Form1 : Form
     {
         private const int Squares = 8;
         private Label[,] labels = new Label[Squares, Squares];
+
+        private readonly string[,] board = new string[8, 8] {
+            { "♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜" },
+            { "♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟" },
+            { "", "", "", "", "", "", "", "" },
+            { "", "", "", "", "", "", "", "" },
+            { "", "", "", "", "", "", "", "" },
+            { "", "", "", "", "", "", "", "" },
+            { "♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙" },
+            { "♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖" }
+        };
+
         public Form1()
         {
             InitializeComponent();
+            float sideW = this.ClientSize.Width / (float)Squares;
+            float sideH = this.ClientSize.Height / (float)Squares;
+            for (int i = 0; i < Squares; i++)
+            {
+                for (int j = 0; j < Squares; j++)
+                {
+                    Point labelPoint = new Point((int)(sideW * i + 20), (int)(sideH * i + 20));
+                    labels[i, j] = new Label
+                    {
+                        Text = board[i, j],
+                        Font = new Font("Segoe UI Symbol", 28),
+                        Location = labelPoint,
+                        Tag = $"{Squares - j}{(char)('A' + (Squares - i - 1))}",
+                        AutoSize = true,
+                        Size = new Size(50, 20),
+
+                        BackColor = Color.Black/*((i + j) % 2 == 1 ? Color.Black : Color.White)*/,
+                        ForeColor = Color.White/*((i + j) % 2 == 1 ? Color.Black : Color.White)*/,
+
+                        Visible = true
+                    };
+                    this.Controls.Add(labels[i, j]);
+                }
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -22,10 +60,9 @@ namespace task1
                 for (int j = 0; j < Squares; j++)
                 {
                     Brush brush = ((i + j) % 2 == 0 ? Brushes.Black : Brushes.White);
-
+                    
                     graphics.DrawRectangle(p, sideW * i, sideH * j, sideW, sideH);
                     graphics.FillRectangle(brush, sideW * i, sideH * j, sideW, sideH);
-
                 }
             }
         }
