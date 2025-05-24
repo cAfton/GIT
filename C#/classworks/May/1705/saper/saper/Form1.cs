2f94e4f0943df449f6b7cc853d902288b62f1314
cAfton <166216153+cAfton@users.noreply.github.com>
2025-05-24 14:45:17 +0300

    save

--- C#/classworks/May/1705/saper/saper/Form1.cs
+++ C#/classworks/May/1705/saper/saper/Form1.cs
@@ -2,9 +2,12 @@ namespace saper
 {
     public partial class Form1 : Form
     {
+        private GameManager gameManager {  get; set; }
         public Form1()
         {
             InitializeComponent();
+            gameManager = new GameManager(10, this);
+            gameManager.StartGame();
         }
 
     }
