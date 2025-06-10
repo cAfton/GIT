11bb744b25f704ee753035c3de8482be5c84c3dc
cAfton <166216153+cAfton@users.noreply.github.com>
2025-06-11 00:00:28 +0300

    models and tables done

---
+++ C#/homeworks/!WindowsFormsHomework/homework7(UML)/Library/BibliothecaManager.cs
@@ -0,0 +1,28 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class BibliothecaManager
+    {
+        public Bibliotheca MainBibliotheca {  get; set; }
+
+        public BibliothecaManager(Bibliotheca bibliotheca) { }
+
+        public void AddNewUser() { }
+
+        public void RemoveUser() { }
+
+        public void AddNewBook() { }
+
+        public void RemoveBook() { }
+
+        public void LendBook() { }
+
+        public void ReturnBook() { }
+    }
+}
---
+++ C#/homeworks/!WindowsFormsHomework/homework7(UML)/Library/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public List<Book> BorrowedBooks {  get; set; }
+    }
+}
